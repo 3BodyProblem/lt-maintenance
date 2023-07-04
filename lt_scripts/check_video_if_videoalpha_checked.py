ed55e310b1d92383a48338592594d85e5bd2abb0
Barry Paneer <barrypaneer@Barry-MacBook-Pro-4-Working.local>
2023-07-04 18:50:29 +0800

    added

---
+++ lt_scripts/check_video_if_videoalpha_checked.py
@@ -0,0 +1,56 @@
+"""
+    Command : `python manage.py lms shell < check_video_if_videoalpha_checked.py`
+
+"""
+
+from django.contrib.auth.models import User
+
+from opaque_keys.edx.keys import CourseKey
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from xmodule.modulestore.django import modulestore
+
+
+print('1) ### Load a user from Mysql...')
+try:
+    _user = User.objects.get(username='LT-developer')
+except Exception:
+    _user = User.objects.get(username='edx')
+print('... user loaded: {}'.format(_user))
+
+
+# INCORRECT_NAME = 'videoalpha'
+# CORRECT_NAME = 'video'
+INCORRECT_NAME = 'video'
+CORRECT_NAME = 'videoalpha'
+
+
+print('2) ### Replacing `videoalpha` with `video`...')
+for course in CourseOverview.objects.all():
+    course_key = course.id
+    try:
+        course_description = modulestore().get_course(course_key)
+    except Exception:
+        print("... WARNING: coruse ({}) doesn't exist in MongoDB...".format(course_key))
+        continue
+
+    for index, module in enumerate(course_description.advanced_modules):
+        if module == INCORRECT_NAME:
+            course_description.advanced_modules[index] = CORRECT_NAME
+            modulestore().update_item(course_description, _user.id)
+            print('... Replaced Course ({})`videoalpha` with `video` ==> {}'.format(
+                course_key, ','.join(course_description.advanced_modules))
+            )
+
+
+print('3) ### Checking `videoalpha` related courses, again...')
+for course in CourseOverview.objects.all():
+    course_key = course.id
+    try:
+        course_description = modulestore().get_course(course_key)
+    except Exception:
+        print("... WARNING: coruse ({}) doesn't exist in MongoDB...".format(course_key))
+        continue
+
+    for module in course_description.advanced_modules:
+        if module == INCORRECT_NAME:
+            print('... `videoalpha` still exists in Course ({}) ==> {}'.format(course_key))
